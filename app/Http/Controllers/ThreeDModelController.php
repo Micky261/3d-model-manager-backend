a4edaec3b9a441830ce0e029fc831355a175a9a4
Frank Nelles <f.nelles@hotmail.com>
2021-01-04 10:30:31 +0100

    Add: Basic ThreeDModel model with api endpoints

---
+++ app/Http/Controllers/ThreeDModelController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ThreeDModel;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ThreeDModelController extends Controller {
+    public function getAllModels(): JsonResponse {
+        return response()->json(ThreeDModel::get());
+    }
+
+    public function createModel(Request $request): JsonResponse {
+        $threeDModel = new ThreeDModel();
+        $threeDModel->name = $request->name;
+        $threeDModel->imported_name = $request->imported_name;
+        $threeDModel->links = $request->links;
+        $threeDModel->save();
+
+        return response()->json($threeDModel);
+    }
+
+    public function getModel(int $id): JsonResponse|Response {
+        if (ThreeDModel::where("id", $id)->exists()) {
+            $threeDModel = ThreeDModel::where("id", $id)->get()[0];
+            return response()->json($threeDModel);
+        } else {
+            return response(status: 404);
+        }
+    }
+
+    public function updateModel(Request $request, int $id): JsonResponse|Response {
+        if (ThreeDModel::where("id", $id)->exists()) {
+            $threeDModel = ThreeDModel::find($id);
+            $threeDModel->name = is_null($request->name) ? $threeDModel->name : $request->name;
+            $threeDModel->links = is_null($request->links) ? $threeDModel->links : $request->links;
+            $threeDModel->save();
+
+            return response()->json($threeDModel);
+        } else {
+            return response(status: 404);
+
+        }
+    }
+
+    public function deleteModel(int $id): JsonResponse|Response {
+        if (ThreeDModel::where("id", $id)->exists()) {
+            $threeDModel = ThreeDModel::find($id);
+            $threeDModel->delete();
+
+            return response()->json($threeDModel);
+        } else {
+            return response(status: 404);
+        }
+    }
+
+    public function getRandomModels(int $num): JsonResponse|Response {
+        if ($num > 0) {
+            $threeDModels = ThreeDModel::select()->inRandomOrder()->limit($num)->get();
+
+            return response()->json($threeDModels);
+        } else {
+            return response(status: 400);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ThreeDModelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware("auth:api")->get("/user", function (Request $request) {
     return $request->user();
 });
+
+Route::get("models", [ThreeDModelController::class, "getAllModels"]);
+Route::get("models/random/{num}", [ThreeDModelController::class, "getRandomModels"]);
+Route::get("model/{id}", [ThreeDModelController::class, "getModel"]);
+Route::post("model", [ThreeDModelController::class, "createModel"]);
+Route::put("model/{id}", [ThreeDModelController::class, "updateModel"]);
+Route::delete("model/{id}", [ThreeDModelController::class, "deleteModel"]);
+
