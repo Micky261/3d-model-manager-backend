5a72edaa610bfee175196d366e3f5936d19d0a2b
Frank Nelles <f.nelles@hotmail.com>
2021-01-09 20:49:12 +0100

    Refactor: TaggedThreeDModel->ModelTags

--- app/Models/ModelTags.php
+++ app/Models/ModelTags.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class TaggedThreeDModel extends Model
+class ModelTags extends Model
 {
     use HasFactory;
 
